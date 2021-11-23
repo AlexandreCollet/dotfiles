3ff8341df4fd9b0610c6edd30fd799e9ce4903ad
Alexandre Collet <3273308+AlexandreCollet@users.noreply.github.com>
2021-11-23 06:11:48 +0000

    install: add script (wip)

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# x11
+ln -sfn $PWD/x11/xresources $HOME/.Xresources
+#sudo ln -sfn $PWD/x11/xorg/* /etc/X11/xorg.conf.d/
+
+# I3
+ln -sfn $PWD/i3/ $HOME/.config/
+
+# Polybar
+ln -sfn $PWD/polybar/ $HOME/.config/
+
+# Rofi
+mkdir -p $HOME/.local/share/applications/
+ln -sfn $PWD/rofi/applications/* $HOME/.local/share/applications/
+
+# Terminal
+ln -sfn $PWD/alacritty/ $HOME/.config/
+ln -sfn $PWD/zsh/zshrc $HOME/.zshrc
+
+# Autorandr
+ln -sfn $PWD/autorandr $HOME/.config/
+
+# Code
+ln -sfn $PWD/code/* $HOME/.config/Code/User/
+ln -sfn $PWD/git/gitconfig $HOME/.gitconfig
